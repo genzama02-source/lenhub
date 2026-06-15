9d9f6c18ea1601fc3cd1e7f82eeebbd72e7a0fb9
genzama02-source <genzama02@gmail.com>
2026-06-15 18:03:03 +0700

    Update lenhub.lua

--- lenhub.lua
+++ lenhub.lua
@@ -1,1 +1,11 @@
+local items = {"Sword", "Shield", "Potion", "Rare Gem"}
+
+function rollItem()
+    local result = items[math.random(1, #items)]
+    print("Kamu mendapatkan: "..result)
+end
+
+return {
+    rollItem = rollItem
+}
 
